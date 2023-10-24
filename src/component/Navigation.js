0d61caa04b6d94012485abef4505b7d043b0e9fa
JangHaeMin <asaw2123@naver.com>
2023-10-24 13:57:50 +0900

    nav commit

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../css/Navigation.css";
 
-const Navigation = ({ navigateToModal }) => {
+const Navigation = ({}) => {
   const [searchList, SetSearchList] = useState([]);
   
   
